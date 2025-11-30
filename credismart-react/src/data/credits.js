cad96129ca23b0db084bd54d0f94a87bb70660bc
Jorim13 <jorim.saltarin@gmail.com>
2025-11-30 12:56:43 -0500

    Array para los creditos

---
+++ credismart-react/src/data/credits.js
@@ -0,0 +1,146 @@
+export const credits = [
+    {
+        id: 1,
+        name: "Credito de libre inversión",
+        description: "Un crédito que puedes usar para cualquier propósito personal.",
+        montoMin: 1000000,
+        montoMax: 50000000,
+        tasaInteres: 1.2,
+        mazTiempo: 60,
+        requirements: [
+            "Ser mayor de 18 años",
+            "Tener ingresos comprobables",
+            "No tener deudas pendientes",
+            "Ingresos mínimos de $1,000,000 mensuales",
+            ],
+        icon: ""
+    },
+    {
+        id: 2,
+        name: "Crédito de Vehículo",
+        description: "Financia la compra de tu vehículo nuevo o usado con tasas competitivas.",
+        montoMin: 5000000,
+        montoMax: 80000000,
+        tasaInteres: 0.95,
+        mazTiempo: 72,
+        requirements: [
+            "Ser mayor de 18 años",
+            "Tener ingresos comprobables",
+            "No tener deudas pendientes",
+            "Ingresos mínimos de $1,000,000 mensuales",
+            ],
+        icon: ""
+    },
+    {
+        id: 3,
+        name: "Crédito de Vivienda",
+        description: "Obtén el financiamiento que necesitas para comprar la casa de tus sueños.",
+        montoMin: 20000000,
+        montoMax: 500000000,
+        tasaInteres: 0.75,
+        mazTiempo: 240,
+        requirements: [
+            "Ser mayor de 18 años",
+            "Tener ingresos comprobables",
+            "No tener deudas pendientes",
+            "Ingresos mínimos de $1,000,000 mensuales",
+            ],
+        icon: ""
+    },
+    {
+        id: 4,
+        name: "Crédito Educativo",
+        description: "Financia tus estudios y alcanza tus metas académicas con nuestro crédito educativo.",
+        montoMin: 2000000,
+        montoMax: 30000000,
+        tasaInteres: 0.85,
+        mazTiempo: 48,
+        requirements: [
+            "Ser mayor de 18 años",
+            "Tener ingresos comprobables",
+            "No tener deudas pendientes",
+            "Ingresos mínimos de $1,000,000 mensuales",
+            ],
+        icon: ""
+    },
+    {
+        id: 5,
+        name: "Crédito Empresarial",
+        description: "Impulsa el crecimiento de tu pequeña o mediana empresa con nuestro crédito PyME.",
+        montoMin: 5000000,
+        montoMax: 100000000,
+        tasaInteres: 1.1,
+        mazTiempo: 84,
+        requirements: [
+            "Ser mayor de 18 años", 
+            "Tener ingresos comprobables",
+            "No tener deudas pendientes",
+            "Ingresos mínimos de $1,000,000 mensuales",
+            ],
+        icon: ""
+    },
+    {
+        id: 6,
+        name: "Crédito Verde",
+        description: "Financia proyectos sostenibles y amigables con el medio ambiente.",
+        montoMin: 3000000,
+        montoMax: 40000000,
+        tasaInteres: 0.9,
+        mazTiempo: 60,
+        requirements: [
+            "Ser mayor de 18 años", 
+            "Tener ingresos comprobables",
+            "No tener deudas pendientes",
+            "Ingresos mínimos de $1,000,000 mensuales",
+            ],
+        icon: ""
+    },
+    {
+        id: 7,
+        name: "Crédito para Viajes",
+        description: "Haz realidad el viaje de tus sueños con nuestro crédito para viajes.",
+        montoMin: 1000000,
+        montoMax: 20000000,
+        tasaInteres: 1.3,
+        mazTiempo: 36,
+        requirements: [
+            "Ser mayor de 18 años",
+            "Tener ingresos comprobables",
+            "No tener deudas pendientes",
+            "Ingresos mínimos de $1,000,000 mensuales",
+            ],
+        icon: ""
+    },
+    {
+        id: 8,
+        name: "Crédito para Salud",
+        description: "Cubre gastos médicos y tratamientos con nuestro crédito para salud.", 
+        montoMin: 2000000,
+        montoMax: 25000000,
+        tasaInteres: 1.15,
+        mazTiempo: 48,
+        requirements: [
+            "Ser mayor de 18 años",
+            "Tener ingresos comprobables",
+            "No tener deudas pendientes",   
+            "Ingresos mínimos de $1,000,000 mensuales",
+            ],
+        icon: ""
+    },
+    {
+        id: 9,
+        name: "Crédito para Renovación del Hogar",
+        description: "Mejora y renueva tu hogar con nuestro crédito para renovación.",  
+        montoMin: 3000000,
+        montoMax: 40000000,
+        tasaInteres: 1.05,
+        mazTiempo: 60,
+        requirements: [
+            "Ser mayor de 18 años",
+            "Tener ingresos comprobables",
+            "No tener deudas pendientes",   
+            "Ingresos mínimos de $1,000,000 mensuales",
+            ],
+        icon: ""
+    }
+]
---
+++ credismart-react/src/pages/Simulador.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Simulador = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Simulador
