2d708a054f72f6bfa000ec6522ef4fa995199c53
Jorim13 <jorim.saltarin@gmail.com>
2025-11-30 13:49:33 -0500

    Componente Navbar

--- credismart-react/src/components/Navbar.jsx
+++ credismart-react/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
+import {Link, useLocation} from 'react-router-dom';
 import React from 'react'
 
 export const Navbar = () => {
-  return (
-    <nav className='navbar'>
-    </nav>
-  )
+
+    const location = useLocation();
+    const isActive = (path) => {
+        return location.pathname === path ? 'active' : '';
+    }
+
+    return (
+        <nav className='navbar'>
+            <div className='container'>
+                <Link to="/" className='nav-brand'>CreditSmart</Link>
+
+                <ul className='nav-menu'>
+                    <li>
+                        <Link to="/" className={isActive('/')}>Inicio</Link>
+                    </li>
+                    <li>
+                        <Link to="/simulator" className={isActive('/simulator')}>Simulador</Link>
+                    </li>
+                    <li>
+                        <Link to="/credits" className={isActive('/credits')}>Créditos</Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className={isActive('/about')}>Nosotros</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
 }
